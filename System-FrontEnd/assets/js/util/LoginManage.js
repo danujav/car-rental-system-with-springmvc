e9468f4964c3d9293ee1f6865227529db6b6d39f
DanujaV <danujagreru@gmail.com>
2021-06-17 05:43:02 +0530

    driver form done

--- System-FrontEnd/assets/js/util/LoginManage.js
+++ System-FrontEnd/assets/js/util/LoginManage.js
@@ -2,14 +2,20 @@ var manageCarSection = document.getElementById('manageCarSection');
 var dashboardSection = document.getElementById('dashboardSection');
 var registerCustomerSection = document.getElementById('registerCustomerSection');
 var loginCustomerSection = document.getElementById('loginCustomerSection');
+var driverManageSection = document.getElementById('driverManageSection');
+var loginDriverSection = document.getElementById('loginDriverSection');
 
 
 manageCarSection.style.display = "none";
 registerCustomerSection.style.display = "none";
 loginCustomerSection.style.display = "none";
+driverManageSection.style.display = "none";
+loginDriverSection.style.display = "none";
 
 var btnSign = document.getElementById('btnSign');
 var btnLogin = document.getElementById('btnLogin');
+var btnDriverSign = document.getElementById('btnDriverSign');
+var btnDriverLogin = document.getElementById('btnDriverLogin');
 
 btnSign.addEventListener('click', function () {
     manageCarSection.style.display = "none";
@@ -23,4 +29,23 @@ btnLogin.addEventListener('click', function () {
     dashboardSection.style.display = "none";
     registerCustomerSection.style.display = "none";
     loginCustomerSection.style.display = "block";
+});
+
+btnDriverSign.addEventListener('click', function () {
+    manageCarSection.style.display = "none";
+    dashboardSection.style.display = "none";
+    registerCustomerSection.style.display = "none";
+    loginCustomerSection.style.display = "none";
+    driverManageSection.style.display = "block";
+    loginDriverSection.style.display = "none";
+
+});
+btnDriverLogin.addEventListener('click', function () {
+    manageCarSection.style.display = "none";
+    dashboardSection.style.display = "none";
+    registerCustomerSection.style.display = "none";
+    loginCustomerSection.style.display = "none";
+    driverManageSection.style.display = "none";
+    loginDriverSection.style.display = "block";
+
 });
--- System-FrontEnd/assets/js/util/NavBar.js
+++ System-FrontEnd/assets/js/util/NavBar.js
@@ -3,16 +3,24 @@ var manageCarSection = document.getElementById('manageCarSection');
 var dashboardSection = document.getElementById('dashboardSection');
 var registerCustomerSection = document.getElementById('registerCustomerSection');
 var loginCustomerSection = document.getElementById('loginCustomerSection');
+var driverManageSection = document.getElementById('driverManageSection');
+var loginDriverSection = document.getElementById('loginDriverSection');btnDriverSign
+
 
 
 manageCarSection.style.display = "none";
 registerCustomerSection.style.display = "none";
 loginCustomerSection.style.display = "none";
+driverManageSection.style.display = "none";
+loginDriverSection.style.display = "none";
 
 
 var carBtn = document.getElementById('manageCar');
 var dashboardBtn = document.getElementById('dashboard');
 var registerCustomerBtn = document.getElementById('registerCustomer');
+var btnRegisterDriver = document.getElementById('registerDriver');
+
+
 
 carBtn.addEventListener('click', function () {
     manageCarSection.style.display = "block";
@@ -27,12 +35,22 @@ dashboardBtn.addEventListener('click', function () {
     loginCustomerSection.style.display = "none";
     dashboardSection.style.display = "block";
 });
-    registerCustomerBtn.addEventListener('click', function () {
+registerCustomerBtn.addEventListener('click', function () {
+    manageCarSection.style.display = "none";
+    dashboardSection.style.display = "none";
+    loginCustomerSection.style.display = "block";
+    registerCustomerSection.style.display = "none";
+    loginDriverSection.style.display = "none";
+});
+btnRegisterDriver.addEventListener('click', function () {
         manageCarSection.style.display = "none";
         dashboardSection.style.display = "none";
-        loginCustomerSection.style.display = "block";
+        loginDriverSection.style.display = "block";
         registerCustomerSection.style.display = "none";
+        loginCustomerSection.style.display = "none";
 });
+
+
 
     /*for dashboard*/
 $( ".card" ).click(function() {
